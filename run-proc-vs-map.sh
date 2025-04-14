7badb13a08048eb3a91599032998b84a9b85070c
Paul E. McKenney <paulmck@kernel.org>
2025-04-14 11:32:33 -0700

    run-proc-vs-map.sh: Add --rawdata argument
    Produce data for gnuplot.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- run-proc-vs-map.sh
+++ run-proc-vs-map.sh
@@ -15,11 +15,13 @@ echo "Running $scriptname $args"
 
 # Default!
 nsamples=7
+rawdata=0
 
 usage () {
 	echo "Usage: $scriptname optional arguments:"
 	echo "       --nsamples (default $nsamples)"
 	echo "       --help"
+	echo "       --rawdata"
 	echo "       -- proc-vs-map.sh options (no default)"
 	exit 1
 }
@@ -35,6 +37,9 @@ do
 			usage
 		fi
 		shift
+		;;
+	--rawdata)
+		rawdata=1
 		;;
 	--help|-h)
 		usage
@@ -57,7 +62,7 @@ do
 	do
 		./proc-vs-map.sh "$@" --nbusycpus $n
 	done
-done 2>&1 | gawk '
+done 2>&1 | gawk -v rawdata="${rawdata}" '
 /^ --- / {
 	nbusytasks = $NF;
 	terminated = 0;
@@ -75,11 +80,13 @@ done 2>&1 | gawk '
 }
 
 END {
-	print "";
-	print "          Average mmap()/munmap()";
-	print "           latency (milliseconds)";
-	print "         --------------------------";
-	print "#Busy    Median   Minimum   Maximum #Hangs";
+	if (!rawdata) {
+		print "";
+		print "          Average mmap()/munmap()";
+		print "           latency (milliseconds)";
+		print "         --------------------------";
+		print "#Busy    Median   Minimum   Maximum #Hangs";
+	}
 	for (i in n) {
 		n1 = asort(a[i]);
 		n2 = asort(m[i]);
@@ -90,7 +97,13 @@ END {
 			med = (a[i][h + 1] + a[i][h]) / 2;
 		else
 			med = a[i][h + 1];
-		printf "%5d %9.3f %9.3f %9.3f %6s\n", i, med, a[i][1], m[i][n1], t[i] ? "*" t[i] : "";
+		if (!rawdata) {
+			printf "%5d %9.3f %9.3f %9.3f %6s\n", i, med, a[i][1], m[i][n1], t[i] ? "*" t[i] : "";
+		} else {
+			print "";
+			for (j = 1; j <= n[i]; j++)
+				printf "%9.3f %9.3f %9.3f\n", med, a[i][j], m[i][j];
+		}
 	}
 	print "";
 }'
