7d2d468086f055807ba7b0770e4b96829cb32c65
Paul E. McKenney <paulmck@kernel.org>
2025-04-14 12:37:02 -0700

    run-latency.sh: Add a script to vary latencies
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

---
+++ run-latency.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in 1 2 5 10
+do
+	echo ./run-proc-vs-map.sh --nsamples 100 --rawdata -- --busyduration $i
+	./run-proc-vs-map.sh --nsamples 100 --rawdata -- --busyduration $i
+done
