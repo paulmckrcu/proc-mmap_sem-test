d176a2f1e6fddd7a71ec84f1fae8c54b2b744953
Paul E. McKenney <paulmck@kernel.org>
2025-06-02 18:44:22 -0700

    run-latency.sh: Run only the two-millisecond variant
    This is a step towards automating the acceptance test.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- run-latency.sh
+++ run-latency.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-for i in 1 2 5 10
-do
-	echo ./run-proc-vs-map.sh --nsamples 100 --rawdata -- --busyduration $i
-	./run-proc-vs-map.sh --nsamples 100 --rawdata -- --busyduration $i
-done
+# for i in 1 2 5 10
+# do
+	i=2
+ 	echo ./run-proc-vs-map.sh --nsamples 100 --rawdata -- --busyduration $i
+ 	./run-proc-vs-map.sh --nsamples 100 --rawdata -- --busyduration $i
+# done
